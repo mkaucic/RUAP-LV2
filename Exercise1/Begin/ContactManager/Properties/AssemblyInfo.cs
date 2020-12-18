3ab8c4b359fcada5cfcc77467d98ecfcea56b156
matija.kaucic <matakaucic@gmail.com>
2020-12-18 18:06:21 +0100

    Task 2 - Creating the Contact Manager API Controllers

--- Exercise1/Begin/ContactManager/Properties/AssemblyInfo.cs
+++ Exercise1/Begin/ContactManager/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3e3aa481-6a2c-4963-82b0-d60358521373")]
+[assembly: Guid("f0fe11d9-c561-455c-af57-328165461438")]
 
 // Version information for an assembly consists of the following four values:
 //
